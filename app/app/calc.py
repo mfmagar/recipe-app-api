ca7d21aadf2ecfcf32e24d8db74dfe53cc1037e9
Mohammed Magar <mfmagar@gmail.com>
2023-03-24 15:57:30 +0530

    Adding db

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add x and y and return result"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result"""
+    return y - x
+
+def multiply(x, y):
+    """Multiply x y times and return result"""
+    return x * y
